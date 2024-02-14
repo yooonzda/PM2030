72ce67873891245d94c240708f3ef18cb4ccaf8b
yooonzda <adznooy@gmail.com>
2024-02-14 21:52:21 +0900

    operator 과제

--- JS/step01/js/1_5_operator.js
+++ JS/step01/js/1_5_operator.js
@@ -1,7 +1,10 @@
 /* 
     ? 기본사칙(숫자열) 연산자
     * +, -, *, / 나누기, % 나머지, () 우선
-    
+    * 10 / 3 = 3
+    * 10 % 3 = 1
+    * 20 % 6 = 2
+
     ? 문자열 연산자
     * + 문자열을 합친다.
 
@@ -14,14 +17,21 @@
 
     ? 증감 연산자
     * ++ 1씩 증가, -- 1씩 감소
+    * number++ / number--
 
     ? 조건비교 연산자
     * >= 좌변이 우변보다 크거나 같다.
     * <= 우변이 좌변보다 크거나 같다.
     * > 좌변이 크다.
     * < 우변이 크다.
+    
     * == 좌변과 우변이 같다.
+    * a = true ; 
+    * a == true ; true or false
+    
     * != 좌변과 우변이 다르다.
+    * 1 == true; true (두개의 값이 같으면 true / 다르면 false)
+    * 1 != true; false (두개의 값이 다르면 true / 같으면 false)
     * ex> 
     if(273 < 52){
         alert('273은 52보다 작습니다.');
@@ -33,14 +43,46 @@
 
     ? 논리 연산자
     * &&(곱) 그리고, ||(합, verticalBar) 이거나
-    
+    * 조건을 판별할때 사용 
+    * 합집합 과 교집합
+    * and / or 
     ? 논리 부정연산자
     * !
     * ex>
-    alert(!true);
-    alert(!false);
+    alert(!true); -> false
+    alert(!false); -> true
     참을 거짓으로, 거짓을 참으로 바꾼다.
 
     ? 접근 연산자
     * .
 */
+
+function numbOP1(){
+    var a = 10;
+    var result = a + 20 + 30;
+    console.log(result);
+}
+
+function numbOP2(){
+    var a = 50;
+    var b = 20;
+    var result = a*b%3; 
+    console.log(result);
+}
+
+function numberPlusAfter(){
+    var number = 10;
+    alert(++number); // 11 -> 10
+    alert(++number); // 12 -> 11
+    alert(++number); // 13 -> 12
+    console.log(number);
+}
+
+function numberPlusTodo(){
+    var number = 10;
+    alert(number++); //? 출력 : / 현재변수 : - 서술. 
+    alert(--number); //? 출력 : / 현재변수 : - 서술. 
+    alert(++number); //? 출력 : / 현재변수 : - 서술. 
+    alert(number--); //? 출력 : / 현재변수 : - 서술. 
+    console.log(number); //? 출력 : / 현재변수 : - 서술. / 최종 number - ?. 
+}
